5f5e9f373178a5c977ad208dd01facc9023d1cfd
Aniket Shival <aniketshival007@gmail.com>
2023-01-05 18:13:36 +0530

    initial commit

---
+++ src/components/ShopName.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ShopName = () => {
+  return (
+    <div  >
+        <p className='display-1 mt-5 d-flex justify-content-center' style={{ color:'red' }} >Game Shop</p>
+        <p className='d-flex mx-5 justify-content-center ' style={{ color:'whitesmoke' }} >Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit illum cum numquam laudantium tenetur voluptatum. Officiis nobis corrupti odio dolorem doloribus sequi soluta, quaerat tenetur quisquam quis similique doloremque eos reprehenderit facere! Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente error veniam aliquam sint dolore, porro dolores a reprehenderit id, amet soluta esse totam, quod unde eius ab deleniti nihil tenetur beatae. Aspernatur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti quod dolores illo, id error saepe magni molestiae praesentium, repudiandae, ducimus voluptatem blanditiis quasi soluta. Incidunt ut quos, ex eos pariatur ipsam nisi excepturi molestiae corporis eligendi illum dignissimos nulla eaque ipsa atque deserunt et unde doloribus facere libero quod. Ea, facilis dolore aut deleniti suscipit quod. Est nostrum necessitatibus, labore cumque ad sit nulla.</p>
+    </div>
+  )
+}
+
+export default ShopName
